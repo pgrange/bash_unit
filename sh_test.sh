06484a8ac19efb52a6846815f88a01f9b62afd30
Pascal Grange <pascal@grange.nom.fr>
2011-07-13 10:14:44 +0200

    first commit

---
+++ sh_test.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+# test utility function
+# to write tests :
+# 1. source this file
+# 2. call run_test_suite when all your test* functions have been defined
+
+run() {
+    TEST=$1
+    echo -n "Running test $TEST... "
+    $TEST
+    echo "SUCCESS"
+}
+
+fail() {
+    MESSAGE=$1
+    echo FAILURE: $MESSAGE
+    exit 1
+}
+
+assert() {
+    ASSERTION=$1
+    MESSAGE=$2
+    eval "$ASSERTION" >/dev/null 2>&1 || fail "$MESSAGE"
+}
+
+assertFail() {
+    ASSERTION=$1
+    MESSAGE=$2
+    eval "$ASSERTION" >/dev/null 2>&1 && fail "$MESSAGE"
+    return $?
+}
+
+assertFailWithStatus() {
+    EXPECTED_STATUS=$1
+    ASSERTION=$2
+    MESSAGE=$3
+    assertFail "$ASSERTION" "$MESSAGE"
+    STATUS=$?
+    [ $EXPECTED_STATUS = "$STATUS" ] || fail "$MESSAGE"
+}
+
+assertEquals() {
+    EXPECTED=$1
+    ACTUAL=$2
+    [ "$EXPECTED" = "$ACTUAL" ] || fail "expected $EXPECTED but was $ACTUAL"
+}
+
+run_test_suite() {
+  for test in $(set | grep '^test' | sed -e 's: .*::')
+  do
+    declare -F | grep ' setup$' >/dev/null && setup
+    run $test
+  done
+}
