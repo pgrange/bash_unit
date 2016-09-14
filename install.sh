f33e636c3671f69b56280497f08ced96dd6cce03
Guillaume Vincent <guillaume@oslab.fr>
2016-09-14 10:34:06 +0200

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "downloading bash_unit"
+current_working_dir=$PWD
+tarball_url=$(curl -s https://api.github.com/repos/guillaumevincent/bash_unit/releases | grep tarball_url | head -n 1 | cut -d '"' -f 4)
+tmp_dir=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpdir'`
+cd $tmp_dir
+curl -Ls $tarball_url | tar -xz
+find $tmp_dir -type f -name "bash_unit" | xargs cp -t $current_working_dir
+rm -rf $tmpdir
+echo "thank you for downloading bash_unit, you can now run ./bash_unit"
