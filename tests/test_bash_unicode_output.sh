d6bbfcb86292e96ef0cc146d4aa4967e9b650bad
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2016-09-22 11:49:12 +0200

    Add some unicode tests
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- tests/test_bash_unicode_output.sh
+++ tests/test_bash_unicode_output.sh
@@ -9,3 +9,20 @@ test_can_fail() {
 test_is_correct() {
    assert true
 }
+
+code() {
+  touch /tmp/the_file
+}
+
+test_code_creates_the_file() {
+  code
+  assert "test -e /tmp/the_file"
+}
+
+code2() {
+  exit 25
+}
+
+test_code_with_code_25() {
+  assert_status_code 25 code2
+}
