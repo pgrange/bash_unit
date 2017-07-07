53733c7991b1f096dd22a39b2a329fbfc169e273
Guillaume Vincent <guillaume@oslab.fr>
2017-07-07 18:24:28 +0200

    Remove warning with find command (#41)
    find: warning: you have specified the -maxdepth option after a non-option argument -type, but options are not positional (-maxdepth affects tests specified before it as well as those specified after it).  Please specify options before other arguments.

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ tarball_url=$(curl -s https://api.github.com/repos/pgrange/bash_unit/releases | 
 tmp_dir=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpdir'`
 cd $tmp_dir
 curl -Ls $tarball_url | tar -xz
-find "${tmp_dir}" -type f -name "bash_unit" -maxdepth 2 -exec cp {} "${current_working_dir}" \;
+find "${tmp_dir}" -maxdepth 2 -type f -name "bash_unit" -exec cp {} "${current_working_dir}" \;
 rm -rf $tmpdir
 echo "thank you for downloading bash_unit, you can now run ./bash_unit"
