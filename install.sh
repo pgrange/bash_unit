5348dbead0cb86bbec80a61976ccb0b6140f3513
Björn Busse <bj.rn@baerlin.eu>
2021-03-03 18:16:24 +0100

    install: Use tar with STDIN explicitly
    This otherwise fails on e.g. FreeBSD where /dev/sa0 is the default

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ current_working_dir=$PWD
 tarball_url=$(curl -s https://api.github.com/repos/pgrange/bash_unit/releases | grep tarball_url | head -n 1 | cut -d '"' -f 4)
 tmp_dir=`mktemp -d 2>/dev/null || mktemp -d -t 'tmpdir'`
 cd $tmp_dir
-curl -Ls $tarball_url | tar -xz
+curl -Ls $tarball_url | tar -xz -f -
 find "${tmp_dir}" -maxdepth 2 -type f -name "bash_unit" -exec cp {} "${current_working_dir}" \;
 rm -rf $tmpdir
 echo "thank you for downloading bash_unit, you can now run ./bash_unit"
